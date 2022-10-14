ef23239109ef57ea7e9b322ee30e4bd76a4ebfb0
Washington Nieto <washington_nieto@hotmail.com>
2022-10-14 16:27:57 -0500

    C# my firstapp

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write("You wrote your first lines of code!");
+Console.WriteLine();
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is teh second line");
+
